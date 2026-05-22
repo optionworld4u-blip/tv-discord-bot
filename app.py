bdb0014ebe012585fad8fcfb7b57ef7a377aa86b
optionworld4u-blip <optionworld4u@gmail.com>
2026-05-22 21:38:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,9 +98,9 @@ def send_summary():
 
             message += (
                 f"{idx}. {a['ticker']}\n"
-                f"Close        : {a['close']:.2f}\n"
+                f"Close         : {a['close']:.2f}\n"
                 f"Cross ↑      : {a['cross']:.2f}\n"
-                f"RSI          : {a['rsi']:.2f}\n\n"
+                f"RSI            : {a['rsi']:.2f}\n\n"
             )
 
         # =========================
